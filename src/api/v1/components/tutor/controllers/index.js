1dfd950edd1614c12173e30e929a62f264387de6
ckonlyvk <ducluuthien@gmail.com>
2022-06-05 21:03:11 +0700

    .

---
+++ src/api/v1/components/tutor/controllers/index.js
@@ -0,0 +1,17 @@
+import createNewTutor from "./createNewTutor";
+import getListTutors from "./getListTutors";
+import getProfile from "./getProfile";
+import searchAllTutors from "./searchAllTutors";
+import updateCertificateImg from "./updateCertificateImg";
+import updateProfile from "./updateProfile";
+import updateVideoIntro from "./updateVideoIntro";
+
+export default {
+  getListTutors,
+  searchAllTutors,
+  createNewTutor,
+  getProfile,
+  updateProfile,
+  updateVideoIntro,
+  updateCertificateImg,
+};
---
+++ src/api/v1/components/users/controllers/index.js
@@ -0,0 +1,19 @@
+import getInfo from "./getInfo";
+import getUsers from "./getUsers";
+import login from "./login";
+import loginFacebook from "./loginFacebook";
+import loginGoogle from "./loginGoogle";
+import refreshToken from "./refreshToken";
+import register from "./register";
+import updateUser from "./updateUser";
+
+export default {
+  register,
+  login,
+  loginFacebook,
+  loginGoogle,
+  refreshToken,
+  getInfo,
+  getUsers,
+  updateUser,
+};
